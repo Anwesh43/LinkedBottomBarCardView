e2b21b04c76444ec34b814af706477b335d263a6
Anwesh43 <anweshthecool0@gmail.com>
2021-01-18 02:25:18 +0530

    adding constants and libraries

--- bottombarcardview/src/main/java/com/example/bottombarcardview/BottomBarCardView.kt
+++ bottombarcardview/src/main/java/com/example/bottombarcardview/BottomBarCardView.kt
@@ -1,2 +1,25 @@
 package com.example.bottombarcardview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.app.Activity
+
+val backColor : Int = Color.parseColor("#BDBDBD")
+val sizeFactor : Float = 2.9f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#009688",
+    "#FF5722",
+    "#FF9800",
+    "#795548"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val barSizeFactor : Float = 9.2f
+val rot : Float = 90f
+val lines : Int = 4
